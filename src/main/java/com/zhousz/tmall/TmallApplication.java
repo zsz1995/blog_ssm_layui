3c9a801febbd362527c80739d5ddc4aeb88b2513
zhousz <13687646803@163.com>
2018-06-08 20:32:30 +0800

    init

---
+++ src/main/java/com/zhousz/tmall/TmallApplication.java
@@ -0,0 +1,12 @@
+package com.zhousz.tmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmallApplication.class, args);
+    }
+}
